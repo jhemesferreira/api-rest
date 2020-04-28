42150774e9157bc1bd3bd3e3ce4deb8b1e50bad2
Jhemes Ferreira <jhemes.ferreira.dla@gmail.com>
2020-04-28 17:39:15 +0200

    Final

--- src/routes/userRoutes.js
+++ src/routes/userRoutes.js
@@ -4,12 +4,11 @@ import loginRequired from '../middlewares/loginRequired';
 
 const router = new Router();
 
-// Não deveria existir em uma aplicação real
-// (não se lista usuarios em uma aplicação, falha de segurança)
-router.get('/', userController.index); // Lista Usuarios
-// router.get('/:id', userController.show); // Lista Usuario
 
-router.post('/', userController.store); // para novos usuarios criarem contas
+// router.get('/', userController.index);
+
+
+router.post('/', loginRequired, userController.store); // para novos usuarios criarem contas
 router.put('/', loginRequired, userController.update); // update
 router.delete('/', loginRequired, userController.delete); // delete
 
